e06433e6eb37e0483ee3f458e723898667721bbe
Adrien Salais <adrien.salais@gmail.com>
2026-05-05 19:57:57 +0200

    ad chdb suppor

--- src/iris/__init__.py
+++ src/iris/__init__.py
@@ -10,4 +10,4 @@ __all__ = ["app", "main"]
 def main() -> None:
     import uvicorn
 
-    uvicorn.run("iris.app:app", host="127.0.0.1", port=8000)
+    uvicorn.run("iris.app:app", host="127.0.0.1", port=8000, workers=1)
